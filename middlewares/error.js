d9649cddff2f19d3c4abf49aa833640fe7a2325a
Svyatoslav Krasnov <beatcazzze@gmail.com>
2020-03-22 17:37:57 +0300

    Eslint no-unused-vars fix

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,6 @@
 const defaultErrorStatus = 500;
 
+// eslint-disable-next-line no-unused-vars
 const errorMiddleware = (err, req, res, next) => {
   const status = err.status || defaultErrorStatus;
   const message = err.message || 'Упс, что-то пошло не так...:(';
